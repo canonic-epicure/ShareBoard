55e7c778d4a8c2c17e72a33b9dba5896ecaa631e
Nickolay Platonov <nickolay@desktop.(none)>
2010-09-02 19:20:02 +0400

    - some progress

--- t/020_vero_basic.t.js
+++ t/020_vero_basic.t.js
@@ -45,7 +45,7 @@ StartTest(function(t) {
     t.diag('Instantiation')
     
     var test = new TestClass({
-        attr2    : 'attr2',
+        attr2   : 'attr2',
         attr3   : 'attr3'
     })
     
@@ -62,10 +62,41 @@ StartTest(function(t) {
     var packet = VERO.history[0]
     
     t.ok(packet.length() == 2, 'Packet has 2 mutations')
+    t.ok(packet.type == 'create', 'Packet type is `create`')
+    
+    var mutation0 = packet.mutations[ 0 ]
+    var mutation1 = packet.mutations[ 1 ]
+    
+    t.ok(mutation0.attributeName == 'attr2' || mutation0.attributeName == 'attr3', 'Order is not guaranteed')
+    t.ok(mutation1.attributeName == 'attr2' || mutation1.attributeName == 'attr3', 'Order is not guaranteed')
 
+    
     //======================================================================================================================================================================================================================================================
-    t.diag('Instantiation')
+    t.diag('Mutation')
+    
+    test.setAttr1('mutate1')
+    test.setAttr2('mutate2')
+    
+    test.commit()
         
+
+    t.ok(VERO.history.length == 2, 'History has 1 packet')
+    
+    var packet = VERO.history[1]
+    
+    t.ok(packet.length() == 2, 'Packet has 2 mutations')
+    
+    var mutation0 = packet.mutations[ 0 ]
+    var mutation1 = packet.mutations[ 1 ]
+    
+    t.ok(mutation0.attributeName == 'attr1', 'Correct attribute name for mutation 0')
+    t.ok(mutation0.oldValue == 'attr1', 'Correct old value for mutation 0')
+    t.ok(mutation0.newValue == 'mutate1', 'Correct new value for mutation 0')
+    
+    t.ok(mutation1.attributeName == 'attr2', 'Correct attribute name for mutation 1')
+    t.ok(mutation1.oldValue == 'attr2', 'Correct old value for mutation 1')
+    t.ok(mutation1.newValue == 'mutate2', 'Correct new value for mutation 1')
+    
         
     t.done()
 })    
