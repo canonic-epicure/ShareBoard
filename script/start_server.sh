467ba25a84426f8f7bdcb65d5c34b51d8eef5652
Nickolay Platonov <nickolay@desktop.(none)>
2011-01-04 19:30:13 +0300

    - some progress on cursor

--- script/start_server.sh
+++ script/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-syncler_server.js --preload=Task.ShareBoard.Model --backendClass=\"KiokuJS.Backend.CouchDB\" --backendParams={\"dbURL\":\"http://local:5984/shareboard\"}
+syncler_server.js --preload=Task.ShareBoard.Model --backendClass=\"KiokuJS.Backend.CouchDB\" --backendParams={\"dbURL\":\"http://local:5984/shareboard\"} --persistLog=false
